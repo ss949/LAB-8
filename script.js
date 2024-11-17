68c218e00d1efb21315c8b63e946c045b45ecb23
Selvia <sn949@njit.edu>
2024-11-16 18:53:56 -0800

    commit
    commit

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Hello World")
+let name="SYlvia" 
+let age=19
+console.log(name)
+console.log(age)
+
+var isFall;
+isFall = true;
+console.log(isFall)
+
+var isNov;
+isNov = true;
+console.log(isNov)
+
+var iscold
+console.log(iscold)
