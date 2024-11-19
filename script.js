da954cf1d0c110d0b1b07c698f5ce0d65afcd5de
Selvia <sn949@njit.edu>
2024-11-18 19:55:55 -0800

    commit
    commit

--- script.js
+++ script.js
@@ -14,3 +14,49 @@ console.log(isNov)
 
 var iscold
 console.log(iscold)
+
+
+let RollNumber=15;
+if (RollNumber>10){
+    console.log("The RollNumber is greater than 10")
+}
+
+
+let isMemeber = true;
+if (isMember) 
+   { console.log("Member discount applied.");
+}
+
+
+let startValue = 50;
+if (startValue > 0){
+    console.log("The startValue is positive");
+} else if (startValue < 0) {
+    console.log("The startValue is negative");
+} else {
+    console.log("The startValue is zero.");
+}
+
+
+let a = 30;
+let b = '30';
+console.log('a == b:', a == b);
+console.log('a === b:', a === b);
+console.log('a != b:', a != b);
+console.log('a > b:', a > b);
+console.log('a < b:', a < b);
+console.log('a >= b:', a >= b);
+console.log('a <= b:', a <= b);
+
+
+let firstName = 'SYlvia';
+let lastName = 'Nage';
+var ageis19;
+let message = "Hello, " + SYlvia + " " + Nage + ", you are " + (19) + " years old."
+{console.log("Hell0, SYlvia Nage, you are 19 years old.");
+}
+
+Number = 50
+if Number>0 and Number<100:
+{print("The number is within range")
+}
